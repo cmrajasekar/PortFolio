8f828bc58fb1f0869255481e520103cf96ecdb7e
cmrajasekar <cmrajasekar19@gmail.com>
2025-06-26 13:18:50 +0530

    Updated sendEmail Feature

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,39 @@
 /*=============== EMAIL JS ===============*/
+const contactForm = document.getElementById("contact-form"),
+  contactMessage = document.getElementById("contact-message");
 
+const sendEmail = (e) => {
+  e.preventDefault();
 
-/*=============== SHOW SCROLL UP ===============*/ 
+  emailjs
+    .sendForm(
+      "service_zrd7g6c",
+      "template_b9jivj8",
+      "#contact-form",
+      "_bZt6NEd60XbJVHVc"
+    )
+    .then(() => {
+      // Show sent message
+      contactMessage.textContent = "Message sent successfully ✅";
 
+      // Remove message after five seconds
+      setTimeout(() => {
+        contactMessage.textContent = "";
+      }, 5000);
+
+      // Clear input fields
+      contactForm.reset();
+    })
+    .catch(() => {
+      // Show error message
+      contactMessage.textContent = "Message not sent (service error) ❌";
+    });
+};
+
+contactForm.addEventListener("submit", sendEmail);
+
+/*=============== SHOW SCROLL UP ===============*/
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
-
 /*=============== SCROLL REVEAL ANIMATION ===============*/
-
